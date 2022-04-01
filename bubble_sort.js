df36fae64814b65865bea5f3f68b20b2bd2f3631
James Meegan <jmeegan2@owlmail.harford.edu>
2022-04-01 16:07:15 -0400

    bubble sort

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,19 +1,19 @@
-const arr = [1,2,5,7,3,8,2,10,1]
+const arr = [1, 2, 5, 7, 3, 8, 2, 10, 1]
 
 let bubbleSort = (arr) => {
-    
+
     //Outer pass
-    for(let i = 0; i < arr.length; i++){
+    for (let i = 0; i < arr.length; i++) {
 
         //inner pass
-        for(let j = 0; j < arr.length - i - 1; j++){
+        for (let j = 0; j < arr.length - i - 1; j++) {
 
             //Vlaue comparison using ascending order 
 
-            if(arr[j+1] < arr[j]){
+            if (arr[j + 1] < arr[j]) {
 
                 //swapping
-                [arr[j+1],arr[j]] = [arr[j],arr[j+1]]
+                [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]]
             }
         }
     };
