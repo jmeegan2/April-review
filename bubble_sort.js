7e2fb4689f0c0200891c2149344c47f0bf98c261
James Meegan <jmeegan2@owlmail.harford.edu>
2022-04-01 16:04:56 -0400

    first commit

---
+++ bubble_sort.js
@@ -0,0 +1,23 @@
+const arr = [1,2,5,7,3,8,2,10,1]
+
+let bubbleSort = (arr) => {
+    
+    //Outer pass
+    for(let i = 0; i < arr.length; i++){
+
+        //inner pass
+        for(let j = 0; j < arr.length - i - 1; j++){
+
+            //Vlaue comparison using ascending order 
+
+            if(arr[j+1] < arr[j]){
+
+                //swapping
+                [arr[j+1],arr[j]] = [arr[j],arr[j+1]]
+            }
+        }
+    };
+    return arr;
+}
+
+console.log(bubbleSort(arr))
